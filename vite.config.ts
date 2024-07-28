bfb48711eaa34365e4d5de5168b2a5b63fd16fee
Bhanu <bhanu.2@cars24.com>
2024-07-29 00:53:13 +0530

    updated config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react' //removed swc
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/atlys-machine-coding/',
     plugins: [react()],
 })
